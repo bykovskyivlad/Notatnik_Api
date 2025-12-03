39d383e7539f25f51743cfb4731da37d1c4ed601
Vladyslav <bikovskijvladislav@gmail.com>
2025-12-03 16:50:02 +0100

    first commit

---
+++ Notatnik_Api/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notatnik_Api.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Notatnik_Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Notatnik_Api/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Notatnik_Api.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }       
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
